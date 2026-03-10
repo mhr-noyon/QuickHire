15065ebf023d0df3281ce83d8e0686f109f68dff
mhr-noyon <mhnoyonmd141@gmail.com>
2026-03-11 02:49:40 +0600

    TiDB database linked

--- backend/dbConfig/db.js
+++ backend/dbConfig/db.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql2");
+const fs = require("fs");
 require("dotenv").config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        ca: fs.readFileSync(process.env.DB_CA),
+    },
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: process.env.CONNECTION_LIMIT || 1,
     port: process.env.DB_PORT || 3306,
 });
 
